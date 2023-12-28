23db0a6668ad4500c8d162cdcf7d1a60843f3b46
Alexandre Francisco <142149179+ALEXANDREFRANCISCO-ID@users.noreply.github.com>
2023-12-28 08:46:32 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,53 @@
+const botoesProximo = document.querySelectorAll('.proximo');
+let aulaAtual = 0;
+
+botoesProximo.forEach((botao, index) => {
+  botao.addEventListener('click', () => {
+    if (index === aulaAtual) {
+      aulaAtual++;
+      mostrarAula(aulaAtual);
+    }
+  });
+});
+
+function mostrarAula(index) {
+  const aulas = document.querySelectorAll('section');
+  aulas.forEach((aula, i) => {
+    if (i === index) {
+      aula.style.display = 'block';
+    } else {
+      aula.style.display = 'none';
+    }
+  });
+
+  atualizarBotoes(aulaAtual);
+}
+
+function atualizarBotoes(index) {
+  botoesProximo.forEach((botao, i) => {
+    if (i === index) {
+      botao.disabled = false;
+    } else {
+      botao.disabled = true;
+    }
+  });
+}
+
+  
+
+
+
+
+
+
+
+  function runCode(elementId) {
+  var codeInput = document.getElementById(elementId + "-input").value;
+  var outputFrame = document.getElementById(elementId + "-output");
+
+  outputFrame.contentWindow.document.open();
+  outputFrame.contentWindow.document.write(codeInput);
+  outputFrame.contentWindow.document.close();
+}
+
+
